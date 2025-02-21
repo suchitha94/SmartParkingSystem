0e8dd2c7407b470bafecf9aa96612acce14d5d6b
suchitha94 <saisuchithab@gmail.com>
2025-02-21 15:45:01 +0530

    Add files via upload

---
+++ ParkingSlot.java
@@ -0,0 +1,25 @@
+package com.example.smartparking.model;
+
+public class ParkingSlot {
+
+	public boolean isAvailable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public double getBasePrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setCurrentPrice(double newPrice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAvailable(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ parkingSlotDao.java
@@ -0,0 +1,5 @@
+package com.example.smartparking.service;
+
+public class parkingSlotDao {
+
+}
